d04cc98111e038adc0aca2da5c8e5d77ecbab289
Hansung Kim <hansung_kim@berkeley.edu>
2026-01-06 16:59:35 -0800

    ubench: Parameterize inst unrolling

--- kernels/ubench/kernel.cpp
+++ kernels/ubench/kernel.cpp
@@ -4,18 +4,28 @@
 int main() {
   vx_tmc(vx_num_threads());
 
+  constexpr int inst_parallelism = 4;
+  constexpr int N = 1000 / inst_parallelism;
+
   int sum0 = 0;
   int sum1 = 0;
   int sum2 = 0;
   int sum3 = 0;
   int incr = 1;
-#pragma unroll 10
-  for (int i = 0; i < 1000; i++) {
+
+#pragma unroll N
+  for (int i = 0; i < N; i++) {
     // sum += i;
     asm volatile("add %0, %0, %1" : "+r"(sum0) : "r"(incr) : "memory");
-    asm volatile("add %0, %0, %1" : "+r"(sum1) : "r"(incr) : "memory");
-    asm volatile("add %0, %0, %1" : "+r"(sum2) : "r"(incr) : "memory");
-    asm volatile("add %0, %0, %1" : "+r"(sum3) : "r"(incr) : "memory");
+    if constexpr (inst_parallelism >= 2) {
+      asm volatile("add %0, %0, %1" : "+r"(sum1) : "r"(incr) : "memory");
+    }
+    if constexpr (inst_parallelism >= 3) {
+      asm volatile("add %0, %0, %1" : "+r"(sum2) : "r"(incr) : "memory");
+    }
+    if constexpr (inst_parallelism >= 4) {
+      asm volatile("add %0, %0, %1" : "+r"(sum3) : "r"(incr) : "memory");
+    }
   }
 
   vx_tmc(1);
