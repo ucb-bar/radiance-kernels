16fce3802e5c38cffd47e8533c076cae5a8a750c
Hansung Kim <hansung_kim@berkeley.edu>
2026-01-12 15:45:15 -0800

    ubench: Parameterize datatype

--- kernels/ubench/kernel.cpp
+++ kernels/ubench/kernel.cpp
@@ -1,35 +1,37 @@
 #include <stdint.h>
+#include <type_traits>
 #include <vx_intrinsics.h>
+
+template <typename T>
+inline T sum() {
+  constexpr int ILP = 16;
+  constexpr int N = 2048 / ILP;
+
+  T sum[ILP] = {static_cast<T>(0),};
+  T incr = 1.;
+
+#pragma unroll 100
+  for (int i = 0; i < N; i++) {
+    // sum += i;
+#pragma unroll ILP
+    for (int j = 0; j < ILP; j++) {
+      if constexpr (std::is_integral_v<T>) {
+        asm volatile("add %0, %0, %1" : "+r"(sum[j]) : "r"(incr) : "memory");
+      } else {
+        asm volatile("fadd.s %0, %0, %1" : "+r"(sum[j]) : "r"(incr) : "memory");
+      }
+    }
+  }
+
+  return sum[0];
+}
 
 int main() {
   vx_tmc(vx_num_threads());
 
-  constexpr int inst_parallelism = 4;
-  constexpr int N = 1000 / inst_parallelism;
-
-  int sum0 = 0;
-  int sum1 = 0;
-  int sum2 = 0;
-  int sum3 = 0;
-  int incr = 1;
-
-#pragma unroll N
-  for (int i = 0; i < N; i++) {
-    // sum += i;
-    asm volatile("add %0, %0, %1" : "+r"(sum0) : "r"(incr) : "memory");
-    if constexpr (inst_parallelism >= 2) {
-      asm volatile("add %0, %0, %1" : "+r"(sum1) : "r"(incr) : "memory");
-    }
-    if constexpr (inst_parallelism >= 3) {
-      asm volatile("add %0, %0, %1" : "+r"(sum2) : "r"(incr) : "memory");
-    }
-    if constexpr (inst_parallelism >= 4) {
-      asm volatile("add %0, %0, %1" : "+r"(sum3) : "r"(incr) : "memory");
-    }
-  }
+  sum<int32_t>();
 
   vx_tmc(1);
 
-  int sum = sum0 + sum1 + sum2 + sum3;
-  return sum;
+  return 0;
 }
