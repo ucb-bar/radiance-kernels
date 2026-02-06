5d99466933347b06536f3459f52fc2b588ec6e47
Yash Kodali <yash.kodali@berkeley.edu>
2026-02-05 19:00:42 -0800

    add barrier single test

--- kernels/barrier/single/kernel.cpp
+++ kernels/barrier/single/kernel.cpp
@@ -5,8 +5,7 @@
 #include "../../memstress_common/helpers.h"
 
 namespace {
-constexpr uint32_t kDefaultIterations = 16;
-constexpr uint32_t kInnerBarriers = 4;
+constexpr uint32_t kDefaultIterations = 64;
 }
 
 void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
@@ -18,18 +17,9 @@ void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
   const uint32_t iterations =
       memstress::resolve_iterations(arg, kDefaultIterations);
   uint32_t acc = static_cast<uint32_t>(task_id);
-
-  // Nested barrier structure
-  // outer barrier id=0 wraps repeated inner barrier id=1
   for (uint32_t iter = 0; iter < iterations; ++iter) {
     vx_barrier(0, vx_num_warps());
-    for (uint32_t inner = 0; inner < kInnerBarriers; ++inner) {
-      vx_barrier(1, vx_num_warps());
-      acc += (iter + inner);
-      vx_barrier(1, vx_num_warps());
-      acc ^= ((iter << 2) ^ inner);
-    }
-    vx_barrier(0, vx_num_warps());
+    acc += iter;
   }
 
   volatile uint32_t* dst = memstress::dst_ptr(arg);
@@ -39,7 +29,6 @@ void kernel_body(int task_id, kernel_arg_t* __UNIFORM__ arg) {
 int main() {
   auto* arg = reinterpret_cast<kernel_arg_t*>(KERNEL_ARG_DEV_MEM_ADDR);
   const uint32_t grid = memstress::resolve_threads(arg);
-  // Use per-core task spawn so 1-core configs can still utilize multiple warps
   vx_spawn_tasks(grid, (vx_spawn_tasks_cb)kernel_body, arg);
   return 0;
 }
