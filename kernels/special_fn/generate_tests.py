78efb6d824c5ab1d259347fdc5261e602f7a74be
joshua <jyou12@berkeley.edu>
2026-02-13 01:08:49 -0800

    sin test

---
+++ kernels/special_fn/generate_tests.py
@@ -0,0 +1,34 @@
+import math
+
+sin_test_vector = [
+    0.0,
+    math.pi / 6,
+    math.pi / 4,
+    math.pi / 3,
+    math.pi / 2,
+    math.pi,
+    3 * math.pi / 2,
+    2 * math.pi,
+
+    1.0e-6,
+    1.0e-10,
+    1.0e-20,
+    1.0e-38,
+    1.4e-45,
+    5.9e-39,
+    -1.0e-6,
+    -1.4e-45,
+
+    100.0,
+    1000.0,
+    1.0e6,
+    1.0e10,
+    -1.0e6,
+
+    -0.0
+]
+
+with open('sin', 'w') as f:
+    for arg in sin_test_vector:
+        expected = math.sin(arg)
+        f.write(f'{float.hex(arg)},{float.hex(expected)},')
--- kernels/special_fn/special_fn.cpp
+++ kernels/special_fn/special_fn.cpp
@@ -1,29 +1,62 @@
-#include <vx_intrinsics.h>
 #include <math.h>
+#include <stdio.h>
 
-// TODO: write these
+static volatile int *cout = (int*)(0xFF080000);
+
+// muon's libc doesn't define PI (??)
+#define PI 3.14159265358979323846264338327950288419716939937510582f
+
+static void puts(const char* str) {
+    while (*str != '\0') {
+        *cout = *str++;
+    }
+    *cout = '\n';
+}
+
+// muon's libc doesn't define PI (??)
+#define PI 3.14159265358979323846264338327950288419716939937510582f
+#define PI_2 1.5707963267948966192313216916397514420985846996875529f
+
+#define EPS 1.0e-6
+
+// special functions don't always give the same answer across platforms
+static void assert_close(float a, float b) {
+  char buf[128];
+  if (fabsf(a - b) > EPS) {
+      puts("bad");
+  }
+}
+
+volatile float sin_test_vector[] = { 
+  #include "sin"
+};
+
+void correctness_sin() {
+  #define CHECK_SIN(arg, expected) {      \
+      float input = (arg);                \
+      volatile float x = sinf(input);     \
+      assert_close(x, expected);          \
+  }
+
+  
+  unsigned int length = sizeof(sin_test_vector) / sizeof(float);
+
+  for (int i = 0; i < length / 2; i += 1) {
+      CHECK_SIN(sin_test_vector[i*2], sin_test_vector[i*2+1]);
+  }
+}
+
 void measure_sin() {
-    // sin of small angles (denormal)
-    // sin of angles within [-2pi, 2pi]
-    // sin of really big angles (forces reduction of angle)
-}
-
-void measure_cos() {
-    // same test vectors as sin
-}
-
-void measure_exp() {
-    // 
+  volatile float input = PI / 17.0f;
+  volatile float x;
+  for (int i = 0; i < 1000; i += 1) {
+    x = sinf(input);
+  }
+  asm("");
 }
 
 int main() {
-  volatile float zero = 0.0f;
-  volatile float cos_zero = cosf(zero);
-  int cos_zero_i = *(int*)(&cos_zero);
-
-  volatile int *cout = (int*)(0xFF080000);
-  *cout = cos_zero_i;
-  *cout = '\n';
-
+  correctness_sin();
+  measure_sin();
   return 0;
 }
