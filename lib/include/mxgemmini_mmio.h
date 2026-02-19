3cb6b78f06cbb0dabea04a8be3ad276eea141295
Hansung Kim <hansung_kim@berkeley.edu>
2026-02-18 22:56:25 -0800

    Fix MMIO API to use shared mem stores

--- lib/include/mxgemmini_mmio.h
+++ lib/include/mxgemmini_mmio.h
@@ -12,18 +12,23 @@
  *  two would be ideal.
  */
 
-#define GEMMINI_SF_MEM 0x00088000
-#define GEMMINI_SF_MEM_A (GEMMINI_SF_MEM + 0x2000)
-#define GEMMINI_SF_MEM_B GEMMINI_SF_MEM
+#include <mu_intrinsics.h>
 
-#define GEMMINI_CTRL 0x00084000
-#define GEMMINI_RS1_ADDR  (GEMMINI_CTRL + 0x10)
-#define GEMMINI_RS2_ADDR  (GEMMINI_CTRL + 0x18)
-#define GEMMINI_INST_ADDR (GEMMINI_CTRL + 0x0)
+#define GEMMINI_SF_MEM    0x00088000
+#define GEMMINI_SF_MEM_A (GEMMINI_SF_MEM + 0x2000)
+#define GEMMINI_SF_MEM_B (GEMMINI_SF_MEM)
+
+#define GEMMINI_CTRL        0x00084000
+#define GEMMINI_RS1_OFFSET  0x10
+#define GEMMINI_RS2_OFFSET  0x18
+#define GEMMINI_INST_OFFSET 0x0
+#define GEMMINI_RS1_ADDR   (GEMMINI_CTRL + GEMMINI_RS1_OFFSET)
+#define GEMMINI_RS2_ADDR   (GEMMINI_CTRL + GEMMINI_RS2_OFFSET)
+#define GEMMINI_INST_ADDR  (GEMMINI_CTRL + GEMMINI_INST_OFFSET)
 
 #undef ROCC_INSTRUCTION_RS1_RS2
 #define ROCC_INSTRUCTION_RS1_RS2(x, rs1, rs2, funct) { \
-    *((volatile uint64_t *) GEMMINI_RS1_ADDR) = (rs1); \
-    *((volatile uint64_t *) GEMMINI_RS2_ADDR) = (rs2); \
-    *((volatile uint32_t*) GEMMINI_INST_ADDR) = (0x7B) | (0 << 7) | (3 << 12) | (1 << 15) | (2 << 20) | ((funct) << 25); \
+    store64_shared(GEMMINI_CTRL, GEMMINI_RS1_OFFSET, rs1); \
+    store64_shared(GEMMINI_CTRL, GEMMINI_RS2_OFFSET, rs2); \
+    store_shared  (GEMMINI_CTRL, GEMMINI_INST_OFFSET, (0x7B) | (0 << 7) | (3 << 12) | (1 << 15) | (2 << 20) | ((funct) << 25)); \
 }
