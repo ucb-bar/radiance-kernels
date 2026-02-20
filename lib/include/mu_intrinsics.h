15a157f011b4f52da509b362857186749ff155e9
Hansung Kim <hansung_kim@berkeley.edu>
2026-02-19 21:07:00 -0800

    Add num_threads to mu_intrinsics

--- lib/include/mu_intrinsics.h
+++ lib/include/mu_intrinsics.h
@@ -2,6 +2,7 @@
 #define __MU_INTRINSICS_H__
 
 #include <stdint.h>
+#include <vx_intrinsics.h>
 
 // Base of GPU GMEM(DRAM) address space in host CPU's global address space
 #define GPU_DRAM_ADDR_BASE 0x100000000ul
@@ -9,6 +10,11 @@
 inline void store_shared(uint32_t base, uint32_t offset, uint32_t data) {
   asm volatile("sw.shared %2, %1(%0)" :: "r"(base), "I"(offset), "r"(data)
                : "memory");
+}
+
+// This compiles to CSR reads which stalls the pipeline. Use sparingly & cache.
+inline int mu_num_threads() {
+    return vx_num_threads();
 }
 
 inline void store64_shared(uint32_t base, uint32_t offset, uint64_t data) {
