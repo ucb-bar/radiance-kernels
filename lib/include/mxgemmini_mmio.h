be7b7969fc8f54fb7a7335d535cda505206097ac
Hansung Kim <hansung_kim@berkeley.edu>
2026-02-26 16:00:08 -0800

    mxgemmini_mmio.h: Support both int/pointer types for rs1/rs2

--- lib/include/mxgemmini_mmio.h
+++ lib/include/mxgemmini_mmio.h
@@ -13,6 +13,7 @@
  */
 
 #include <mu_intrinsics.h>
+#include <type_traits>
 
 #define GEMMINI_SF_MEM    0x00088000
 #define GEMMINI_SF_MEM_A (GEMMINI_SF_MEM + 0x2000)
@@ -26,9 +27,20 @@
 #define GEMMINI_RS2_ADDR   (GEMMINI_CTRL + GEMMINI_RS2_OFFSET)
 #define GEMMINI_INST_ADDR  (GEMMINI_CTRL + GEMMINI_INST_OFFSET)
 
+template <typename T>
+inline uint64_t gemmini_arg_to_u64(T value) {
+    if constexpr (std::is_pointer_v<T>) {
+        return static_cast<uint64_t>(reinterpret_cast<uintptr_t>(value));
+    } else {
+        static_assert(std::is_integral_v<T> || std::is_enum_v<T>,
+                      "Gemmini argument must be integral/enum or pointer");
+        return static_cast<uint64_t>(value);
+    }
+}
+
 #undef ROCC_INSTRUCTION_RS1_RS2
 #define ROCC_INSTRUCTION_RS1_RS2(x, rs1, rs2, funct) { \
-    store64_shared(GEMMINI_CTRL, GEMMINI_RS1_OFFSET, rs1); \
-    store64_shared(GEMMINI_CTRL, GEMMINI_RS2_OFFSET, rs2); \
+    store64_shared(GEMMINI_CTRL, GEMMINI_RS1_OFFSET, gemmini_arg_to_u64(rs1)); \
+    store64_shared(GEMMINI_CTRL, GEMMINI_RS2_OFFSET, gemmini_arg_to_u64(rs2)); \
     store_shared  (GEMMINI_CTRL, GEMMINI_INST_OFFSET, (0x7B) | (0 << 7) | (3 << 12) | (1 << 15) | (2 << 20) | ((funct) << 25)); \
 }
