232fe92859682cdbc28b1185a302cd7076742d85
Shashank Anand <shashank.anand@berkeley.edu>
2026-03-01 18:12:53 -0800

    softmax datagen script

---
+++ kernels/softmax/softmax.py
@@ -0,0 +1,32 @@
+import numpy as np
+from string import Template
+
+ROWS = 4
+COLS = 256
+
+x = np.random.randn(ROWS, COLS).astype(np.float32)
+
+template = Template(
+"""
+__global float x[] = {
+    $x_literals
+};
+
+const uint32_t softmax_rows = $rows;
+const uint32_t softmax_cols = $cols;
+"""
+)
+
+x_literals = ""
+for i in range(ROWS):
+    for j in range(COLS):
+        x_literals += f"{x[i, j]}f,"
+
+with open("data", "w") as f:
+    f.write(
+        template.substitute(
+            x_literals=x_literals,
+            rows=ROWS,
+            cols=COLS,
+        )
+    )
