d5e05f94d0462f04739a6b47c6b13ff59e35e6d5
Hansung Kim <hansung_kim@berkeley.edu>
2026-03-04 13:57:22 -0800

    mu_schedule: Change warp mapping to be core-round-robin

--- lib/src/mu_schedule.cpp
+++ lib/src/mu_schedule.cpp
@@ -24,26 +24,45 @@ struct Context {
 static Context schedule_context[NUM_CORES_MAX];
 
 static void __attribute__ ((noinline)) mu_schedule_standalone() {
-    uint32_t global_core_id = vx_core_id(); // TODO
-    uint32_t global_tid = 0;
-    asm volatile("csrr %0, mhartid" : "=r"(global_tid));
+    uint32_t cluster_id = 0;
+    // TODO: clid not supported in assembler
+    asm volatile("csrr %0, %1" : "=r"(cluster_id) : "i"(MU_CSR_CLUSTER_ID));
+    const auto core_id_in_cluster = vx_core_id();
+    const auto cores_per_cluster = vx_num_cores();
+    const auto global_core_id = cluster_id * cores_per_cluster + core_id_in_cluster;
 
-    const uint32_t threads_per_threadblock =
-        vx_num_cores() * vx_num_warps() * vx_num_threads();
-    const uint32_t tid_in_threadblock = global_tid % threads_per_threadblock;
-    const uint32_t threadblock_id = global_tid / threads_per_threadblock;
+    // thread_id mapping is core-round-robin, i.e. warp 4k+0 maps to core 0,
+    // warp 4k+1 maps to core 1, etc.
+    const auto warp_id_in_core = vx_warp_id();
+    const auto warp_id_rr_in_cluster = (warp_id_in_core * cores_per_cluster) + core_id_in_cluster;
+    const auto tid_in_warp = vx_thread_id();
+    const auto tid_in_cluster = warp_id_rr_in_cluster * MU_NUM_THREADS + tid_in_warp;
+    const auto threads_per_cluster =
+        cores_per_cluster * vx_num_warps() * MU_NUM_THREADS;
+    const auto tid_global = threads_per_cluster * cluster_id + tid_in_cluster;
 
-    const auto &context = schedule_context[global_core_id];
+    // 1-threadblock-to-1-cluster
+    const uint32_t threads_per_threadblock = threads_per_cluster;
+    const uint32_t tid_in_threadblock = tid_global % threads_per_threadblock;
+    const uint32_t threadblock_id = tid_global / threads_per_threadblock;
+
+    const auto &context = schedule_context[core_id_in_cluster];
     const auto callback = context.callback;
     auto arg = context.arg;
 
     callback(arg, tid_in_threadblock, threads_per_threadblock, threadblock_id);
 }
 
-static void __attribute__ ((noinline)) mu_schedule_workers() {
+static void mu_schedule_workers() {
     vx_tmc(-1);
     mu_schedule_standalone();
     vx_tmc_zero();
+}
+
+static void mu_schedule_manager() {
+    vx_tmc(-1);
+    mu_schedule_standalone();
+    vx_tmc(1);
 }
 
 /** Entry point that "bootstraps" kernel via a single thread elected in every
@@ -63,15 +82,9 @@ void mu_schedule(mu_schedule_callback callback, void *arg) {
 
     // TODO: fence here?
 
-    // schedule worker threads & retire
+    // schedule worker threads & manager thread
     vx_wspawn(num_warps, mu_schedule_workers);
-
-    // schedule main thread & return single-threaded
-    {
-        vx_tmc(-1);
-        mu_schedule_standalone();
-        vx_tmc(1);
-    }
+    mu_schedule_manager();
 
     // TODO: add threadblock barrier
 }
