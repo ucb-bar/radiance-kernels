b37f452d39c2a08b618ee8685eaedccb3b8642aa
Hansung Kim <hansung_kim@berkeley.edu>
2026-03-25 11:31:20 -0700

    requant: Make writes to requant in-order by gating only warp 0

--- kernels/gemm_mxgemmini/requant.cpp
+++ kernels/gemm_mxgemmini/requant.cpp
@@ -26,24 +26,27 @@ void requant_entry(void *arg, uint32_t tid_in_threadblock,
     mxgemm_single_output_tile<C>(C.TILE_M, C.TILE_N, C.TILE_K,
                                  tid_in_threadblock);
 
-    auto C_gmem = reinterpret_cast<uint8_t *>(0x40000000);
+    auto C_gmem = reinterpret_cast<uint8_t *>(0x60000000);
 
+    const auto tid_in_warp = tid_in_threadblock % MU_NUM_THREADS;
     const auto warp_id = tid_in_threadblock / MU_NUM_THREADS;
     const auto warps_per_threadblock = threads_per_threadblock / MU_NUM_THREADS;
     mu_barrier(3, warps_per_threadblock);
 
-    constexpr auto N = C.TILE_M * C.TILE_N; // in bf16
-    auto C_out_bf16_gmem = reinterpret_cast<const uint8_t *>(&C_out_bf16[0][0]);
-    const auto smem_requant_base =
-        reinterpret_cast<__shared uint8_t *>(GEMMINI_REQUANT);
-    // store_smem<N, /*zero_stride=*/false>(smem_requant_base,
-    //                                      static_cast<uint16_t>(0x3f80));
-    copy_gmem_to_smem_simt_bf16<C.TILE_M, C.TILE_N, sizeof(C_out_bf16[0][0])>(
-        C_out_bf16_gmem, smem_requant_base, tid_in_threadblock,
-        threads_per_threadblock);
+    if (warp_id == 0) {
+        constexpr auto N = C.TILE_M * C.TILE_N; // in bf16
+        auto C_out_bf16_gmem = reinterpret_cast<const uint8_t *>(&C_out_bf16[0][0]);
+        const auto smem_requant_base =
+            reinterpret_cast<__shared uint8_t *>(GEMMINI_REQUANT);
+        // store_smem<N, /*zero_stride=*/false>(smem_requant_base,
+        //                                      static_cast<uint16_t>(0x3f80));
+        copy_gmem_to_smem_simt_bf16<C.TILE_M, C.TILE_N, sizeof(C_out_bf16[0][0])>(
+            C_out_bf16_gmem, smem_requant_base, tid_in_warp /* trick as one core */,
+            MU_NUM_THREADS /* trick as one core */);
+    }
 }
 
 int main() {
-    mu_schedule(requant_entry, nullptr, 2);
+    mu_schedule(requant_entry, nullptr, 1);
     return 0;
 }
