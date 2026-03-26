8eb4876a5d3958885653a6e8b0ca71c6b9da34cf
Hansung Kim <hansung_kim@berkeley.edu>
2026-03-25 22:14:27 -0700

    mxgemm: Read out C_scale_mem for verifiable trace

--- kernels/gemm_mxgemmini/mxgemm.fp8.singletile.tm64tn64tk64.fp8out.cpp
+++ kernels/gemm_mxgemmini/mxgemm.fp8.singletile.tm64tn64tk64.fp8out.cpp
@@ -22,6 +22,19 @@ void mxgemm_entry(void *arg, uint32_t tid_in_threadblock,
     auto C_gmem = reinterpret_cast<uint8_t *>(0x40000000);
     mxgemm<C>(C.TILE_M, C.TILE_N, C.TILE_K, C_gmem, tid_in_threadblock,
               threads_per_threadblock, threadblock_id);
+
+    const auto warp_id = threads_per_threadblock % MU_NUM_THREADS;
+    const auto warps_per_threadblock = threads_per_threadblock / MU_NUM_THREADS;
+    mu_barrier(4, warps_per_threadblock);
+
+    // generate verifiable trace for the scale factor output
+    if (warp_id == 0) {
+        const auto tid_in_warp = tid_in_threadblock % MU_NUM_THREADS;
+        auto C_scale_gmem = reinterpret_cast<uint8_t *>(0x50000000);
+        copy_gmem_to_gmem_simt<C.TILE_M, C.TILE_N / 32, sizeof(uint8_t)>(
+            reinterpret_cast<const uint8_t *>(&C_scale_factors[0]),
+            C_scale_gmem, tid_in_warp, MU_NUM_THREADS);
+    }
 }
 
 int main() {
