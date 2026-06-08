042b1657c65f5f255be7934d6124c31366e4ff89
Hansung Kim <hansung_kim@berkeley.edu>
2026-06-08 11:31:32 -0700

    gemm_simt: Add disabled host launch code

--- kernels/gemm_simt/host.cpp
+++ kernels/gemm_simt/host.cpp
@@ -1,3 +1,18 @@
+#include <radiance.h>
+
 int main() {
+#if 0
+    tohost = 0;
+    *tocpu = tohost;
+
+    WRITE_MMIO_32(RAD_HOST_GPU_RESET, 0);
+
+    uint32_t finished = 0;
+    while (!finished) {
+        SYNC_GPU();
+        finished = READ_MMIO_32(RAD_HOST_GPU_ALL_FINISHED);
+    }
+#endif
+
     return 0;
 }
